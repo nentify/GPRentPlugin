1835c47e824cd9944d01399400cc201cc8e7028c
nentify <nentify@fastmail.com>
2016-08-26 18:27:27 +0100

    Remove debug/testing code

--- src/main/java/me/nentify/gprent/events/BlockEventHandler.java
+++ src/main/java/me/nentify/gprent/events/BlockEventHandler.java
@@ -3,20 +3,14 @@ package me.nentify.gprent.events;
 import me.nentify.gprent.GPRent;
 import me.nentify.gprent.claims.RentableClaim;
 import me.nentify.gprent.commands.LetCommand;
-import me.nentify.gprent.data.GPRentKeys;
 import me.nentify.gprent.data.rent.ImmutableRentData;
 import me.nentify.gprent.data.rent.RentData;
 import me.ryanhamshire.griefprevention.claim.Claim;
 import ninja.leaping.configurate.ConfigurationNode;
-import ninja.leaping.configurate.commented.CommentedConfigurationNode;
 import org.spongepowered.api.block.BlockSnapshot;
 import org.spongepowered.api.block.tileentity.Sign;
 import org.spongepowered.api.block.tileentity.TileEntity;
 import org.spongepowered.api.data.Transaction;
-import org.spongepowered.api.data.key.Key;
-import org.spongepowered.api.data.key.Keys;
-import org.spongepowered.api.data.manipulator.ImmutableDataManipulator;
-import org.spongepowered.api.data.manipulator.mutable.tileentity.SignData;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.block.ChangeBlockEvent;
@@ -34,7 +28,6 @@ import org.spongepowered.api.world.World;
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 import java.util.UUID;
 
 public class BlockEventHandler {
@@ -120,67 +113,18 @@ public class BlockEventHandler {
         List<Transaction<BlockSnapshot>> transactions = event.getTransactions();
 
         for (Transaction<BlockSnapshot> transaction : transactions) {
-            //Optional<Location<World>> blockLoc = transaction.getOriginal().getLocation();
-
-            //GPRent.instance.logger.info(blockLoc.toString());
-
-            //if (blockLoc.isPresent()) {
-                //Optional<TileEntity> tileEntity = blockLoc.get().getTileEntity();
-
-                //GPRent.instance.logger.info(tileEntity.toString());
-
-                //if (tileEntity.isPresent() && tileEntity.get() instanceof Sign) {
-                    //Sign sign = (Sign) tileEntity.get();
-
-                    //GPRent.instance.logger.info(sign.toString());
-
-                    //Optional<RentData> rentDataOptional = sign.getOrCreate(RentData.class);
-
-            Optional<List<Text>> texts = transaction.getOriginal().get(Keys.SIGN_LINES);
-            System.out.println(texts);
-
-            Optional<String> test = transaction.getOriginal().get(GPRentKeys.CLAIM_ID);
-            System.out.println(test);
-
-            Set<Key<?>> keys = transaction.getOriginal().getKeys();
-            System.out.println(keys);
-
-            List<ImmutableDataManipulator<?, ?>> a = transaction.getOriginal().getContainers();
-            System.out.println(a);
-
             Optional<ImmutableRentData> immutableRentData = transaction.getOriginal().get(ImmutableRentData.class);
 
             if (immutableRentData.isPresent()) {
-                GPRent.instance.logger.info(immutableRentData.get().getClaimId());
-
                 UUID uuid = UUID.fromString(immutableRentData.get().getClaimId());
 
                 Optional<RentableClaim> rentableClaimOptional = GPRent.getRentableClaims().get(uuid);
 
                 if (rentableClaimOptional.isPresent()) {
-                            RentableClaim rentableClaim = rentableClaimOptional.get();
-                            rentableClaim.delete();
-                        }
+                    RentableClaim rentableClaim = rentableClaimOptional.get();
+                    rentableClaim.delete();
+                }
             }
-
-
-//            if (claimUuid.isPresent()) {
-//                        GPRent.instance.logger.info(claimUuid.get());
-//
-//                        UUID uuid = UUID.fromString(claimUuid.get());
-//
-//                        Optional<RentableClaim> rentableClaimOptional = GPRent.getRentableClaims().get(uuid);
-//
-//                        if (rentableClaimOptional.isPresent()) {
-//                            RentableClaim rentableClaim = rentableClaimOptional.get();
-//                            rentableClaim.delete();
-//                        }
-//                    }
-
-
-
-                //}
-            //}
         }
     }
 }
